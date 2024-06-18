015e71be2cb4665c0ad2393c57bb9cc299fe2308
luckyriver-blue <yoshi.midori@icloud.com>
2024-06-18 13:00:01 +0900

    口コミ作成システムの基本

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -3,16 +3,35 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\Hospital_Department;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
+    public function Hospitals()
+    {
+        return view('hospitals.hospitals')->with(['hospitals' => $hospital->getHospitalsPaginateByLimit]);
+    }
     public function HospitalReview(Post $post)
     {
-        return view('posts.hospital_review')->with(['posts' => $post->getPaginateByLimit()]);
+        return view('hospitals.posts.hospital_review')->with(['posts' => $post->getPaginateByLimit()]);
     }
     public function ShowReview(Post $post)
     {
         return view('posts.show_review')->with(['post' => $post]);
+    }
+    public function create(Request $request)
+    {
+        return view('hospitals.posts.create');
+    }
+    public function OptionDepartment(Hospital_Department $hospital_department)
+    {
+        return view('hospitals.posts.create')->with(['hospital_departments' => $hospital_department->get()]);
+    }
+    public function store(Request $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
     }
 }
