461e6c84828694a0e411bbd3f12ca94e8f1d6f74
luckyriver-blue <yoshi.midori@icloud.com>
2024-07-07 21:22:18 +0900

    ヘッダー

--- resources/views/hospitals/top.blade.php
+++ resources/views/hospitals/top.blade.php
@@ -17,35 +17,29 @@
             }
         </style>
     </head>
-    <body class="antialiased">
-        <div>
-            <p align=right>ログイン</p>
-                <h1>病院口コミサイト</h1>
-                <h2>
-                    ホーム
-                    検索
-                    口コミを書く
-                    マイページ
-                </h2>
-        <div>
-        <hr>
-        <div>
-            <div class='search'>
-                <form action="/hospitals" method="GET">
-                    <input type="text" name="keyword" value="{{ $keyword }}" placeholder="病院名、場所、診療科で病院検索">
-                    <input type="submit" value ="検索">
-                </form>
+    <x-app-layout>
+        <x-slot name="header">
+            トップ
+        </x-slot>
+        <body class="antialiased">
+            <div>
+                <div class='search'>
+                    <form action="/hospitals" method="GET">
+                        <input type="text" name="keyword" value="{{ $keyword }}" placeholder="病院名、場所、診療科で病院検索">
+                        <input type="submit" value ="検索">
+                    </form>
+                </div>
+                <hr>
+                <div class="star_hospitals">
+                    @foreach ($hospitals as $key => $hospital)
+                        <h2 class='name'>
+                                <a href="/hospitals/{{ $hospital->id }}">{{ $hospital->name }}</a>
+                                <span>{{ $key + 1 }}位</span>
+                        </h2>
+                    @endforeach
+                    <a href="/hospitals">・・・</a>
+                </div>
             </div>
-            <hr>
-            <div class="star_hospitals">
-                @foreach ($hospitals as $key => $hospital)
-                    <h2 class='name'>
-                            <a href="/hospitals/{{ $hospital->id }}">{{ $hospital->name }}</a>
-                            <span>{{ $key + 1 }}位</span>
-                    </h2>
-                @endforeach
-                <a href="/hospitals">・・・</a>
-            </div>
-        </div>
-    </body>
+        </body>
+    </x-app-layout>
 </html>
