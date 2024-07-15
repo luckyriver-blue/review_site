497a9bace2a716aa8e82e915e54cb29dd5f314f8
luckyriver-blue <yoshi.midori@icloud.com>
2024-07-16 00:51:52 +0900

    responsive design

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -74,11 +74,20 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('top')" :active="request()->routeIs('top')">
+                        {{ __('トップ') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('hospitals')" :active="request()->routeIs('hospitals')">
+                {{ __('検索') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('create')" :active="request()->routeIs('create')">
+                        {{ __('口コミを書く') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('mypage', ['user' => Auth::user()->id])" :active="request()->routeIs('mypage')">
+                        {{ __('マイページ') }}
             </x-responsive-nav-link>
         </div>
-
+                  
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
